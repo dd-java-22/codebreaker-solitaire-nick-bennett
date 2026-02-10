b1f75ce4dae0a8ead8fa5550ac6708f316299546
Nick Bennett <nick@nickbenn.com>
2026-02-10 13:53:48 -0700

    ONe option for handling a successfully submitted guess.

--- src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/GameViewModel.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/GameViewModel.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
 
-@SuppressWarnings("UnusedReturnValue")
+@SuppressWarnings({"UnusedReturnValue", "CallToPrintStackTrace"})
 public class GameViewModel {
 
   private final CodebreakerService service;
@@ -58,13 +58,54 @@ public class GameViewModel {
         .exceptionally(this::logError);
   }
 
+  public void getGame(String gameId) {
+    service
+        .getGame(gameId)
+        .thenAccept(this::setGame)
+        .exceptionally(this::logError);
+  }
+
+  public void deleteGame(String gameId) {
+    service
+        .delete(gameId)
+        .exceptionally(this::logError);
+  }
+
+  public void deleteGame() {
+    service
+        .delete(game.getId())
+        .thenRun(() -> setGame(null))
+        .exceptionally(this::logError);
+  }
+
+  public void submitGuess(String text) {
+    Guess guess = new Guess.Builder()
+        .text(text)
+        .build();
+    service
+        .submitGuess(game.getId(), guess)
+        .thenAccept((guessResponse) -> {
+          setGuess(guessResponse);
+          //noinspection DataFlowIssue
+          game.getGuesses().add(guessResponse);
+          setGame(game);
+        })
+        .exceptionally(this::logError);
+  }
+
   // TODO: 2026-02-10 Add methods to get and delete game, submit and get guess.
 
   public void registerGameObserver(Consumer<Game> observer) {
     gameObservers.add(observer);
   }
 
-  // TODO: 2026-02-10 Add registration methods for guess observers and error observers.
+  public void registerGuessObserver(Consumer<Guess> observer) {
+    guessObservers.add(observer);
+  }
+
+  public void registerErrorObserver(Consumer<Throwable> observer) {
+    errorObservers.add(observer);
+  }
 
   private Void logError(Throwable error) {
     //noinspection ThrowableNotThrown
