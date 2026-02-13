e2e1536e2db73d7c1d5f91184dc9c6e3d5ad8ec3
Nick Bennett <nick@nickbenn.com>
2026-02-13 09:22:05 -0700

    Tweaking FXML for text-mode round-trip.

--- src/main/java/edu/cnm/deepdive/codebreaker/controller/MainController.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/MainController.java
@@ -2,11 +2,14 @@ package edu.cnm.deepdive.codebreaker.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class MainController {
 
+  @FXML
+  private ScrollPane scrollPane;
   @FXML
   private Text gameState;
   @FXML
