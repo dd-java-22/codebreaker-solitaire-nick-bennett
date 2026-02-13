fa347ee1ed863e9cacd3fb3861ea31a482dfeb83
Nick Bennett <nick@nickbenn.com>
2026-02-13 10:57:33 -0700

    Round-trip working.

--- src/main/java/edu/cnm/deepdive/codebreaker/controller/MainController.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/MainController.java
@@ -1,15 +1,27 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class MainController {
 
+  private static final String PROPERTIES_FILE = "game.properties";
+  private static final String POOL_KEY = "pool";
+  private static final String LENGTH_KEY = "length";
+
   @FXML
   private ScrollPane scrollPane;
+  @FXML
+  private TextFlow textFlow;
   @FXML
   private Text gameState;
   @FXML
@@ -17,8 +29,44 @@ public class MainController {
   @FXML
   private Button send;
 
+  private GameViewModel viewModel;
+  private Game game;
+
+  @FXML
+  private void initialize() throws IOException {
+    textFlow
+        .heightProperty()
+        .addListener((_, _, _) -> scrollPane.setVvalue(1.0));
+    viewModel = connectToViewModel();
+    startGame();
+  }
+
   @FXML
   private void submitGuess() {
+    String guessText = guessInput.getText().strip();
+    if (guessText.length() == game.getLength()) {
+      viewModel.submitGuess(guessText);
+    }
+  }
+
+  private GameViewModel connectToViewModel() {
+    GameViewModel viewModel = GameViewModel.getInstance();
+    viewModel.registerGameObserver((game) -> {
+      this.game = game;
+      gameState.setText(game.toString());
+    });
+    viewModel.registerErrorObserver((throwable) -> { /* TODO Display or log this throwable. */ });
+    return viewModel;
+  }
+
+  private void startGame() throws IOException {
+    try (InputStream input = getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
+      Properties properties = new Properties();
+      properties.load(input);
+      String pool = properties.getProperty(POOL_KEY);
+      int length = Integer.parseInt(properties.getProperty(LENGTH_KEY));
+      viewModel.startGame(pool, length);
+    }
   }
 
 }
