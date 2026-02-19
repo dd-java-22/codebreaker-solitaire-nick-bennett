755d0e81b835e134d88b61cdb08e564d3dc367b5
Nick Bennett <nick@nickbenn.com>
2026-02-19 10:59:33 -0700

    Starting on GuessAdapter.

---
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/client/adapter/GuessAdapter.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive.codebreaker.client.adapter;
+
+import edu.cnm.deepdive.codebreaker.api.model.Guess;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+public class GuessAdapter implements Callback<ListView<Guess>, ListCell<Guess>> {
+
+  @Override
+  public ListCell<Guess> call(ListView<Guess> guessListView) {
+    return null;
+  }
+  
+}
