d870986367fb3a5f50809cad5d54a417440c180b
Nick Bennett <nick@nickbenn.com>
2026-02-19 17:05:45 -0700

    Finished adapter; LOTS of CSS work remains.

--- javafx/src/main/java/edu/cnm/deepdive/codebreaker/client/adapter/GuessAdapter.java
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/client/adapter/GuessAdapter.java
@@ -1,14 +1,19 @@
 package edu.cnm.deepdive.codebreaker.client.adapter;
 
 import edu.cnm.deepdive.codebreaker.api.model.Guess;
+import edu.cnm.deepdive.codebreaker.client.util.CodePointInfo;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
@@ -68,9 +73,32 @@ public class GuessAdapter implements Callback<ListView<Guess>, ListCell<Guess>> 
         setGraphic(null);
       } else {
         // TODO: 2026-02-19 Manipulate nodes referenced by the @FXML-annotated fields to present our model state.
+        List<Node> children = guessContent.getChildren();
+        children.clear();
+        CodePointInfo info = CodePointInfo.getInstance();
+        guess
+            .getText()
+            .codePoints()
+            .mapToObj((codePoint) -> {
+              return buildGuessCharacterItem(codePoint, info);
+            })
+            .forEach(children::add);
         exactCount.setText(String.valueOf(guess.getExactMatches()));
         nearCount.setText(String.valueOf(guess.getNearMatches()));
         setGraphic(root);
+      }
+    }
+
+    private Label buildGuessCharacterItem(int codePoint, CodePointInfo info) {
+      try {
+        FXMLLoader loader = new FXMLLoader(characterLayoutLocation, resources);
+        Label label = loader.load();
+        label.setUserData(codePoint);
+        label.setTooltip(new Tooltip(info.getName(codePoint)));
+        label.getStyleClass().add(info.getStyleClass(codePoint));
+        return label;
+      } catch (IOException e) {
+        throw new RuntimeException(e);
       }
     }
 
