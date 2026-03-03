81d70ba03891cbcbcf557b6d6da082006f5732b6
Nick Bennett <nick@nickbenn.com>
2026-03-02 17:05:15 -0700

    Link GameViewModel with MainActivity
    Integrated GameViewModel into MainActivity to observe game state and initiate a new game, enabling interaction with the ViewModel through UI elements.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/MainActivity.java
@@ -2,8 +2,10 @@ package edu.cnm.deepdive.codebreaker.app.controller;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.codebreaker.app.databinding.ActivityMainBinding;
+import edu.cnm.deepdive.codebreaker.app.viewmodel.GameViewModel;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
+    GameViewModel viewModel = new ViewModelProvider(this).get(GameViewModel.class);
+    viewModel
+        .getGame()
+        .observe(this, (game) -> binding.response.setText(game.toString()));
+    binding.test.setOnClickListener((v) -> viewModel.startGame("ABCDEF", 6));
   }
 
 }
