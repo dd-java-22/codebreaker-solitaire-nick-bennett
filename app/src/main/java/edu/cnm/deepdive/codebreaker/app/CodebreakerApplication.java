eeddd152a5c9b9cde49646745d75cd27b0093b4d
Nick Bennett <nick@nickbenn.com>
2026-03-08 23:25:47 -0600

    Change by Junie: Implement service shutdown in CodebreakerApplication
    Modified CodebreakerApplication to inject GameSummaryService and invoke its shutdown method during onTerminate. This ensures the singleton service is properly closed when the application lifecycle ends. Also confirmed that GameViewModel does not redundantly shut down the service.
    
    Prompt: Make your recommended changes to CodebreakerApplication.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/CodebreakerApplication.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/CodebreakerApplication.java
@@ -2,14 +2,25 @@ package edu.cnm.deepdive.codebreaker.app;
 
 import android.app.Application;
 import dagger.hilt.android.HiltAndroidApp;
+import edu.cnm.deepdive.codebreaker.app.repository.GameSummaryService;
+import jakarta.inject.Inject;
 
 @HiltAndroidApp
 public class CodebreakerApplication extends Application {
+
+  @Inject
+  GameSummaryService summaryService;
 
   // Invoked when application loads.
   @Override
   public void onCreate() {
     super.onCreate();
+  }
+
+  @Override
+  public void onTerminate() {
+    summaryService.shutdown();
+    super.onTerminate();
   }
 
 }
